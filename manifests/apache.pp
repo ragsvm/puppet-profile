fa7d7eaebf179732be66e39c8a3e96037796c290
rags ubu lap <rags@me.com>
2019-05-01 01:24:19 +0530

    added apache.pp

---
+++ manifests/apache.pp
@@ -0,0 +1,13 @@
+class profile::apache(
+   Boolean $default_vhost = false,
+   Integer $port = 80,
+   String $docroot = '/var/www/test',
+  ){
+    class { '::apache':
+      default_vhost => $default_vhost,
+    }
+    ::apache::vhost { 'rags.rags.com':
+      port    => $port,
+      docroot => $docroot,
+    }
+  }
